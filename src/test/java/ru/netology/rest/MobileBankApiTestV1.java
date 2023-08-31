f1edf47a0a2222680717d7980a199fe7ef238dd9
Neto test <sysl86@mail.ru>
2023-08-31 20:09:37 +0400

    Initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+      // Given - When - Then
+      // Предусловия
+        ResponseAwareMatcher<Response> hasSize;
+        given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/demo/accounts")
+      // Проверки
+      .then()
+          .statusCode(200);
+
+    }
+}
